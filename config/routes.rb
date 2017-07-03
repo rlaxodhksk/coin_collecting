d353baf5a48fcb65313c060e40e311df54c83509
TKim <rlaxodhksk@naver.com>
2017-07-03 10:23:45 +0900

    facebook_login

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+    devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
     root 'home#index'
     get 'home/index'
     get 'game' => 'home#game', as: :game
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
